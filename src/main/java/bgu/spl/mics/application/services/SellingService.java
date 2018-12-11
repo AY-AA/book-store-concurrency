47636d2a861a7c4154e4b945d91e54174b1c0e30
avishaiyaniv <avishaiy@post.bgu.il>
2018-12-11 17:51:25 +0200

    Merge branch 'master' of https://github.com/alexabramov2610/SPL_ass2
    # Conflicts:
    #	src/main/java/bgu/spl/mics/application/BookStoreRunner.java

--- src/main/java/bgu/spl/mics/application/services/SellingService.java
+++ src/main/java/bgu/spl/mics/application/services/SellingService.java
@@ -1,5 +1,6 @@
 package bgu.spl.mics.application.services;
 
+import bgu.spl.mics.Event;
 import bgu.spl.mics.Future;
 import bgu.spl.mics.MicroService;
 import bgu.spl.mics.application.messages.*;
@@ -18,6 +19,7 @@ import bgu.spl.mics.application.passiveObjects.*;
 public class SellingService extends MicroService {
 
     private MoneyRegister _moneyRegister;
+    private int _currTick;
 
     public SellingService(String name) {
         super(name);
@@ -30,13 +32,16 @@ public class SellingService extends MicroService {
             terminate();
         });
 
+        subscribeBroadcast(TickBroadcast.class,tickEV->{
+            _currTick = tickEV.getCurrenTick();
+        });
         subscribeEvent(BookOrderEvent.class, ev -> {
 
-            Future<Integer> isAvailable = sendEvent(new CheckAvailabilityEvent(ev.get_bookToOrder()));
+            Future<Integer> isAvailable = sendEvent(new CheckAvailabilityEvent(ev.get_bookToOrderTitle()));
             if (isAvailable != null) {
                 Integer price = isAvailable.get(); //waits until resolved
                 if (price <= ev.get_customer().getAvailableCreditAmount()) {
-                    Future<OrderResult> isTaken = sendEvent(new TakeBookEvent(ev.get_bookToOrder()));
+                    Future<OrderResult> isTaken = sendEvent(new TakeBookEvent(ev.get_bookToOrderTitle()));
                     if (isTaken != null) {
                         OrderResult taken = isTaken.get();
                         if (taken == OrderResult.SUCCESSFULLY_TAKEN) {
@@ -44,11 +49,25 @@ public class SellingService extends MicroService {
                            String address = ev.get_customer().getAddress();
                            int distance = ev.get_customer().getDistance();
                            sendEvent(new DeliveryEvent(address,distance));
+                           OrderReceipt orderReceipt = createReceipt(ev.get_customer(),ev.get_bookToOrderTitle(),ev.get_bookToOrderPrice());
+                           complete(ev,orderReceipt);
                         }
                     }
                 }
             }
         });
+    }
+
+    private OrderReceipt createReceipt(Customer c, String bookTitle, int bookPrice){
+        String seller = this.getName();
+        int customerId = c.getId();
+        String _bookTitle = bookTitle;
+        int _bookPrice = bookPrice;
+        int issuedTick = 1;
+        int orderTick = 1;
+        int processTick = 1;
+        OrderReceipt orderReceipt = new OrderReceipt(0,seller,customerId,bookTitle,bookPrice,issuedTick,orderTick,processTick);
+        return orderReceipt;
     }
 }
 
